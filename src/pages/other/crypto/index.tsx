343c3d3327cdb87589a0818c3e9d9e45797db66b
卢浩鹏 <LuHaoPeng@users.noreply.github.com>
2019-09-25 13:39:08 +0800

    crypto layout

--- src/pages/other/crypto/index.tsx
+++ src/pages/other/crypto/index.tsx
@@ -1,13 +1,37 @@
-import { Col, Layout, Row } from 'antd'
+import { Button, Card, Col, Input, Layout, Row } from 'antd'
 import React from 'react'
 import './index.less'
+
+const { TextArea } = Input
 
 const OtherCrypto: React.FunctionComponent = (): JSX.Element => {
   return (
     <Layout.Content className='page-crypto'>
-      <Row>
-        <Col span={12}>1</Col>
-        <Col span={12}>2</Col>
+      <Row gutter={16}>
+        <Col span={12}>
+          <Card title='加密' bordered={false}>
+            <TextArea rows={4} placeholder='原文' />
+            <Button className='coder-btn' type='primary' icon='arrow-down' />
+            <TextArea
+              className='result-box'
+              rows={4}
+              placeholder='密文'
+              readOnly={true}
+            />
+          </Card>
+        </Col>
+        <Col span={12}>
+          <Card title='解密' bordered={false}>
+            <TextArea rows={4} placeholder='密文' />
+            <Button className='coder-btn' type='primary' icon='arrow-down' />
+            <TextArea
+              className='result-box'
+              rows={4}
+              placeholder='原文'
+              readOnly={true}
+            />
+          </Card>
+        </Col>
       </Row>
     </Layout.Content>
   )
